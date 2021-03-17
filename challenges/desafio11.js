e9706ec766a8f6e29df591f5871fd185f760c181
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-03-17 13:57:56 -0300

    consertando desafios 3, 11 e 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,3 +1,3 @@
-db.produtos.updateMany({}, { $addToSet: { tags: { $each: ["combo", "tasty"], $sort: 1 } } });
+db.produtos.updateMany({}, { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } });
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,12 +1,12 @@
 db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany(
-  { tags: { $all: ["bovino"] } },
-  { avaliacao: { $inc: { avaliacao: 5 } } },
+  { tags: { $in: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany(
-  { tags: { $all: ["ave"] } },
+  { tags: { $in: ["ave"] } },
   { $inc: { avaliacao: 3 } },
 );
 
