fc0084978c660412a86764678e42505bf77adeab
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-17 18:02:20 -0300

    coreeçao desafio 8 e desafio 9

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -2,7 +2,7 @@ db.produtos.updateMany({
   nome: "Quarteirão com Queijo",
 },
 {
-  $pop: { ingredientes: 1 },
+  $pop: { ingredientes: -1 },
 });
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
