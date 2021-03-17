e468814d0c56ff7aa1a96953d96b0f4367d78cd4
Renato Silva <renatosdev@gmail.com>
2021-03-17 16:43:45 -0300

    Codeclimate2 e desafios 3 e 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+  {
+    nome: "Big Mac",
+  },
+  { $currentDate: {
+    ultimaModificacao: { $type: "date" },
+  },
+  },
+  { upsert: true },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: true,
+    },
+  },
+  { nome: 1, _id: 0 },
+);
