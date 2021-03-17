69b6a4fd11997d42988ecebb006de93c76b88d33
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-17 19:15:23 -0300

    desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,11 @@
+db.getCollection("produtos")
+  .updateMany(
+    {},
+    { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+  );
+
+db.getCollection("produtos")
+  .find(
+    {},
+    { nome: 1, valoresNutricionais: 1, _id: 0 },
+  );
