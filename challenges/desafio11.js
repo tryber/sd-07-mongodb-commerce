2d81b51e46892ecea313f9d05f45792addeda0a4
Bruno <bruno_grb@hotmail.com>
2021-03-17 16:44:40 -0300

    desafio 11 pronto

---
+++ challenges/desafio11.js
@@ -0,0 +1,21 @@
+db.produtos.updateMany(
+  {},
+  { $push: {
+    tags: {
+      $each: [
+        "combo",
+        "tasty",
+      ],
+    }
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+);
