c52c53d28175ab99b4f5feda67758b2f0e1520ef
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-17 18:45:33 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,23 @@
+db.getCollection("produtos")
+  .updateMany(
+    {},
+    { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+  );
+
+db.getCollection("produtos")
+  .updateMany(
+    { nome: "Big Mac" },
+    { $inc: { "vendasPorDia.3": 60 } },
+  );
+
+db.getCollection("produtos")
+  .updateMany(
+    { $and: [{ tags: ["bovino", "pão"] }, { tags: "pão" }] },
+    { $inc: { "vendasPorDia.6": 120 } },
+  );
+
+db.getCollection("produtos")
+  .find(
+    {},
+    { nome: 1, vendasPorDia: 1, _id: 0 },
+  );
