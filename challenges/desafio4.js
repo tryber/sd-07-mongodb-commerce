f723dea46fc823f6fea8d46123a7b1fa2ddb00f0
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-03-17 10:19:48 -0300

    lint linttttt

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,3 @@
-db.produtos.updateOne({ nome: "Big Mac"}, { $currentDate: { ultimaModificacao: true } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
 
-db.produtos.find({ultimaModificacao: { $exists: true}}, { _id: 0, nome: 1 });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
