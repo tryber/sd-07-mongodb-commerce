0b3af2c241ea9dc2de9943c0a312141c4c0a8e46
Francisco <franciscoliratorres@gmail.com>
2021-03-17 17:46:37 -0300

    add desafios 10, 11 e 12

---
+++ challenges/desafio10.js
@@ -0,0 +1,34 @@
+db.produtos.updateMany(
+  { },
+  { $push: {
+    vendasPorDia: {
+      $each: [0, 0, 0, 0, 0, 0, 0],
+    } },
+  },
+);
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $inc: {
+    "vendasPorDia.3": 60,
+  },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    $and: [
+      { tags: "bovino" },
+      { tags: "pão" },
+    ],
+  },
+  { $inc: {
+    "vendasPorDia.6": 120,
+  },
+  },
+);
+
+db.produtos.find(
+  { },
+  { nome: 1, vendasPorDia: 1, _id: 0 },
+);
