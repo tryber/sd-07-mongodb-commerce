620b6c610540511beaa1af4b18ea61601b3e8187
CarolSi <a.carolg1@gmail.com>
2021-03-17 18:44:52 -0300

    requisito 1

---
+++ challenges/desafio14.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany({
+  $and: [
+    { "valoresNutricionais.tipo": "sódio" },
+    { "valoresNutricionais.percentual": { $gt: 20, $lt: 40 } },
+  ] },
+{ $addToSet: { tags: "contém sódio" } });
+
+db.produtos.find({},
+  { _id: 0, nome: 1, tags: 1 });
