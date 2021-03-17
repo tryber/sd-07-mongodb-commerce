15872fbb59c954f50fd6e99a479b89a0c6c72034
Filipi Firmino <contatoic3web@gmail.com>
2021-03-17 17:40:26 -0300

    Challenge 1 and 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,6 @@
+// Update data unit value
+db.produtos.updateMany({}, 
+    { $set: { valorUnitario: NumberDecimal("0.00") }});
+
+// Find unit value all product
+db.produtos.find({},{valorUnitario: 1, nome:1, _id: 0}); 
