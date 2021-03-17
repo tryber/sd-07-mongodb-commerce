c6d58b2932bb91bda063be75af99bb19650dd4c7
renatomak <renato.mark.silva@gmail.com>
2021-03-17 15:55:41 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } },
+    },
+  },
+  {
+    $push: {
+      tags: {
+        $each: ["muito sódio"],
+      },
+    },
+  },
+  { upsert: true }
+);
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
