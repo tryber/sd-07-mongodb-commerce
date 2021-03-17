bdc1e8e00f40614dcfd8fc019b4ea66450c8adcc
vitorAmorims <vtamorims@gmail.com>
2021-03-17 08:52:28 -0300

    desafio10

---
+++ challenges/desafio10.js
@@ -0,0 +1,21 @@
+const diasDaSemana = Array(7).fill(0);
+
+db.produtos.updateMany(
+  {},
+  { $set: { vendasPorDia: diasDaSemana } },
+);
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino", "pão"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, vendasPorDia: 1, _id: 0 },
+).pretty();
