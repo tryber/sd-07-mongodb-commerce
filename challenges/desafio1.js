3c36b5ecb1653f21b9db8e1f1235cc10dc19f436
Filipi Firmino <contatoic3web@gmail.com>
2021-03-17 16:59:36 -0300

    First commit and create files chelenge

---
+++ challenges/desafio1.js
@@ -0,0 +1,6 @@
+// Insert data created by
+db.produtos.updateMany({}, {$set: {criadoPor: "Ronald McDonald"}});
+
+// Find name and product creator
+
+db.produtos.find({},{criadoPor: 1, nome:1, _id: 0});
