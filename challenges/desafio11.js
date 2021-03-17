a358c38d7d11bae7b07b9fb6f74d8c8d304544a9
Bruno <bruno_grb@hotmail.com>
2021-03-17 17:19:16 -0300

    corrigindo lint desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,14 +1,12 @@
 use("commerce");
 db.produtos.updateMany(
   {},
-  { $push: {
-    tags: {
-      $each: [
-        "combo",
-        "tasty",
-      ],
-      $sort: 1,
-    },
+  {
+    $push: {
+      tags: {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
     },
   },
 );
