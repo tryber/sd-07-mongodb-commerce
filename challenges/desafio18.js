59ad4fd123fe0e84b9089f9118938161128adb1b
vitorAmorims <vtamorims@gmail.com>
2021-03-17 16:47:17 -0300

    correçao sintaxe desafio 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,6 +1,6 @@
 db.produtos.createIndex(
   { descricao: "text" },
-  { default_language: "portuguese" }
+  { default_language: "portuguese" },
 );
 
-db.produtos.count({ $text: { $search: '"feito com"' } });
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
