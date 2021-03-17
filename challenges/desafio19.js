05f0b1790000e4f690f61543a40c2a4d6914ba79
alvarovascon <alvarovascon2@gmail.com>
2021-03-17 17:29:23 -0300

    correção 19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -3,4 +3,4 @@ db.produtos.updateMany(
   { $rename: { descricao: "descricaoSite" } },
 );
 
-db.produtos.find({}, { _id: 0, descricao: 1, descricaoSite: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 });
