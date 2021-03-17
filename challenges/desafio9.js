6efc3b70050f04e6eae9a71efd1217f75f3e06e2
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-17 18:03:48 -0300

    desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,11 @@
+db.getCollection("produtos")
+  .updateMany(
+    { nome: "Cheddar McMelt" },
+    { $pop: { ingredientes: 1 } },
+  );
+
+db.getCollection("produtos")
+  .find(
+    {},
+    { nome: 1, ingredientes: 1, _id: 0 },
+  );
