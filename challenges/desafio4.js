9106e5466b1ba4b5e87bb495c1cab217e538b18b
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-17 17:35:37 -0300

    desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({ nome: "Big Mac" }, { $currentDate: { lastModified: true, ultimaModificacao: { $type: "date" } } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: false, nome: true });
