c59662cbfe7f04127522e52c4d481611824ed3f7
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-17 15:34:30 -0300

    Adicionando requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,16 +1,6 @@
-/*
-Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken,
-garantindo que não haja duplicidade nos ingredientes.
-Para isso, escreva no arquivo desafio5.js duas queries, nesta ordem:
-Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches menos
- o McChicken, garantindo que não haja duplicidade nos ingredientes.
-Crie uma query que retorne o nome e ingredientes de todos os documentos.
-*/
+db.produtos.updateMany(
+  { nome: { $not: { $eq: "McChicken" } } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
 
-db.produtos
-  .find({ nome: { $not: { $eq: "McChicken" } } }, { _id: 0, nome: 1 })
-  .pretty();
-
-db.produtos
-  .find({ nome: { $not: { $eq: "McChicken" } } }, { _id: 0, nome: 1 })
-  .pretty();
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 }).pretty();
