5116fd1b3a0e753940666775f82c3a430fb1e285
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-17 17:32:37 -0300

    Adicionando requisito 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: { $elemMatch: { percentual: { $gte: 40 } } },
+  },
+  {
+    $push: { tags: "muito sódio" },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 }).pretty();
