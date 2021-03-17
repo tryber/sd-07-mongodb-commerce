978b4dae66bc7bf27422027274857cc673386929
TIAGO <tiagoesdrasvieira@gmail.com>
2021-03-17 17:16:31 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: { $eq: "sódio" },
+      percentual: { $gte: 40 },
+    },
+  },
+},
+{
+  $push: {
+    tags: "muito sódio",
+  },
+});
+db.produtos.find({}, { _id: false, nome: true, tags: true });
