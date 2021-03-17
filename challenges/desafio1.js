b09c4b9d5d5362f2d74b72cd0fb307134c98d54f
nevesfreire <nando.neves@gmail.com>
2021-03-17 11:35:02 -0300

    1-3

---
+++ challenges/desafio1.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: { criadoPor: "Ronald McDonald" },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: 1,
+    criadoPor: 1,
+    _id: false,
+  },
+);
