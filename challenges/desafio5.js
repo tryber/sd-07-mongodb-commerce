346d87324f54e1f6b8320bef67da18d3b33d55a9
Lfocosi <luccafocosi@gmail.com>
2021-03-17 15:22:38 -0300

    desafio 5 correção

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({ nome: { $not: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } });
+db.produtos.updateMany({ nome: { $nin: ["McChicken"] } }, { $addToSet: { ingredientes: "ketchup" } });
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
