585b2e17099c3a97d292643e17d3e38eed4391a1
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-17 18:32:49 -0300

    mais desafios

---
+++ challenges/desafio12.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({}, {
+  $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } });
+
+db.produtos.find({}, { _id: false, nome: true, valoresNutricionais: true });
