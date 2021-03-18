8fc3a7ca53c015815e15513113fb8a96f68aa510
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-03-18 13:52:50 -0300

    desafio17 e 18, add language

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,11 +1,12 @@
 db.produtos.createIndex(
   { descricao: "text" },
+  { default_language: "portuguese" },
 );
 
 db.produtos.countDocuments({
   $text: {
     $search: "frango hamburguer",
-    $language: "none",
+    $language: "portuguese",
     $caseSensitive: false,
   },
 });
