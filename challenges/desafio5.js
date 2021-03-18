c87e55dc6103a91afe7398394d2291678c83989a
Strongreen <strongreen@live.com>
2021-03-18 07:35:36 -0300

    requisito 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,12 @@
+/** @format */
+
+db.produtos.updateMany(
+  { nome: { $not: { $eq: "McChicken" } } },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
