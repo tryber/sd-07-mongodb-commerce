361e6d4743ae40cb2d7751972ffa56ad40bb8709
Strongreen <strongreen@live.com>
2021-03-17 22:15:54 -0300

    requisito 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,38 @@
+/** @format */
+
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      avaliacao: NumberInt("0"),
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    tags: {
+      $in: ["bovino"],
+    },
+  },
+  {
+    $inc: {
+      avaliacao: NumberInt("5"),
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    tags: {
+      $in: ["ave"],
+    },
+  },
+  {
+    $inc: {
+      avaliacao: NumberInt("3"),
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
