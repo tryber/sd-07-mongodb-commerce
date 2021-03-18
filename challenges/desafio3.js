23bbeeae10a1590f8997db85d52c42d4c18becac
Filipi Firmino <contatoic3web@gmail.com>
2021-03-18 11:30:55 -0300

    challenge 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -3,3 +3,7 @@ db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
 
 // Increase more 5
 db.produtos.update({ tags: { $in: ["bovino"] } }, { $inc: { avaliacao: 5 } });
+// 
+db.produtos.update({ tags: { $in: ["ave"] } }, { $inc: { avaliacao: 3 } });
+// 
+db.produtos.find({}, {nome: 1, avaliacao:1, _id:0})
