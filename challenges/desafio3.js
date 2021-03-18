2c02fbfe7178ee9248cbdc406b0a62c008eaa6c7
Filipi Firmino <contatoic3web@gmail.com>
2021-03-17 21:37:11 -0300

    fix error lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,7 +1,5 @@
 // creates evaluation field
-db.produtos.updateMany({}, { $set: {avaliacao: NumberInt("0")}});
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
 
 // Increase more 5
-db.produtos.update({tags:{$in: ["bovino"]}},
-  {$inc: {avaliacao: 5}},
-)
+db.produtos.update({ tags: { $in: ["bovino"] } }, { $inc: { avaliacao: 5 } });
