428977b65c1d1a6843d3951f27be644b976cc233
paula-jfe <paula.jfe@gmail.com>
2021-03-17 22:56:23 -0300

    desafio 10

---
+++ desafio10.js
@@ -0,0 +1,3 @@
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
--- desafio9.js
+++ desafio9.js
@@ -1,2 +1,4 @@
 db.produtos.updateOne({ nome: "Cheddar McMelt" }, { $pop: { ingredients: 1 } });
 db.produtos.find({ }, { nome: 1, ingredients: 1, _id: 0 });
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
+db.produtos.find({ }, { nome: 1, vendasPorDia: 1, _id: 0 });
