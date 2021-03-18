f89109a3b6ad4321cf34396504e965df0a02e28d
Filipi Firmino <contatoic3web@gmail.com>
2021-03-18 18:38:50 -0300

    challenge 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,13 +1,11 @@
 db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 
-db.produtos.updateOne(
-  { nome: "Big Mac" },
-  { $push: { vendasPorDia: { $each: [60], $position: 3 } } },
-);
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+//
 
 db.produtos.updateOne(
   { tags: { $in: ["bovino", "pão"] } },
-  { $push: { vendasPorDia: { $each: [120], $position: 7 } } },
+  { $inc: { "vendasPorDia.7": 120 } },
 );
 
-b.produtos.find({}, { nome: 1, _id: 0, vendasPorDia: 1 });
+db.produtos.find({}, { nome: 1, _id: 0, vendasPorDia: 1 });
