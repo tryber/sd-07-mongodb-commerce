c2d869fd69f341c59dd8034ff9a0857910ecfa4e
Filipi Firmino <contatoic3web@gmail.com>
2021-03-18 22:20:03 -0300

    challenges 17,18,19

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,5 +1,6 @@
-db.produtos.createIndex({descricao: "text"},{default_language:"portuguese"});
-
-db.produtos.countDocuments(
-  {$text: {$search: "frango hamburguer"}}
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
 );
+
+db.produtos.countDocuments({ $text: { $search: "frango hamburguer" } });
