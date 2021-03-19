7cd2c06026738810c5a72fa15a9388e95ba3a258
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-19 18:12:06 -0300

    ate o 9

---
+++ challenges/desafio5.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany({
+  $and: [
+    { nome: { $ne: "McChicken" } },
+    { ingredientes: { $not: { $elemMatch: { $eq: "ketchup" } } } },
+  ],
+}, {
+  $push: { ingredientes: "ketchup" },
+});
+db.produtos.find({}, {
+  nome: 1, ingredientes: 1, _id: 0,
+});
