3ae812e1e49ea6675b40d28962ca075d5806f24d
Filipi Firmino <contatoic3web@gmail.com>
2021-03-18 21:57:30 -0300

    add comma

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -7,7 +7,7 @@ db.produtos.updateMany(
       },
     },
   },
-  { $push: { tags: "contém sódio" } }
+  { $push: { tags: "contém sódio" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
