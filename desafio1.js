01b30c3f76797ba1035917d86a4918ae8846e02a
muriloPereira006 <muriloedu006@gmail.com>
2021-03-19 01:31:47 -0300

    missing trailing comma

--- desafio1.js
+++ desafio1.js
@@ -10,6 +10,6 @@ db.produtos.find(
   {
     _id: 0,
     nome: 1,
-    criadoPor: 1
+    criadoPor: 1,
   },
 );
--- desafio2.js
+++ desafio2.js
@@ -8,6 +8,6 @@ db.produtos.find(
   {
     _id: 0,
     nome: 1,
-    valorUnitario: 1
+    valorUnitario: 1,
   },
 );
