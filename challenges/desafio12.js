6ea804c6dfeefb7f2eda2a4734892f1f5889d284
LAURO DE SA <itrafor@gmail.com>
2021-03-20 19:28:19 -0300

    Desafio 12 corrigido

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,19 +1,19 @@
 db.produtos.updateMany(
-    {},
-    {
-      $push: {
-        tags: {
-          $each: [],
-          $sort: {
-            percentual: -1,
-            },
+  {},
+  {
+    $push: {
+      tags: {
+        $each: [],
+        $sort: {
+          percentual: -1,
         },
       },
     },
-  );
+  },
+);
 
 db.produtos.find({}, {
-    nome: 1,
-    valoresNutricionais: 1,
-    _id: 0,
+  nome: 1,
+  valoresNutricionais: 1,
+  _id: 0,
 });
