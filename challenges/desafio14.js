731b7240d8a5e0169798b733b28fce90bab3bad7
muriloPereira006 <muriloedu006@gmail.com>
2021-03-20 18:15:14 -0300

    lint desafi014

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,8 +1,8 @@
 db.produtos.updateMany(
   { valoresNutricionais: { $elemMatch: {
-      tipo: "sódio",
-      percentual: { $gt: 20, $lt: 40 },
-    } } },
+    tipo: "sódio",
+    percentual: { $gt: 20, $lt: 40 },
+  } } },
   { $addToSet: { tags: "contém sódio" } },
 );
 
