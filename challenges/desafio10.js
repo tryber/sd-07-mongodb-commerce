dbc63019851d02280d1a5f504f8ac4052e2321e0
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-20 15:16:51 -0300

    10-16 fix

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,5 @@
-db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
-db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia[2]": 60 } });
-db.produtos.updateMany({ tags: { $elemMatch: { $eq: { $in: ["bovino", "pão"] } } } }, { $inc: { "vendasPorDia[6]": 120 } });
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
+db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } },
+  { upsert: true });
+db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
