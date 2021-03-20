d4fe176ceb83a4f349e0e670f4c493c228abf88d
lisvalver <lisvalver.atte@gmail.com>
2021-03-19 23:11:02 -0300

    Desafio 01

---
+++ challenges/desafio1.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      criadoPor: "Ronald McDonald",
+    },
+  },
+  { upsert: true },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    criadoPor: 1,
+  },
+);
