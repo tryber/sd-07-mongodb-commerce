9668da1469ea532491f6983b5000baec68c73604
LAURO DE SA <itrafor@gmail.com>
2021-03-20 17:47:36 -0300

    Desafio 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,14 +1,12 @@
 db.produtos.updateMany({
-    nome: "Cheddar McMelt",
-  }, {
-    $pop: {
-      ingredientes: 1,
-    },
-  });
-  
-  db.produtos.find({}, {
-    nome: 1,
+  nome: "Cheddar McMelt",
+}, {
+  $pop: {
     ingredientes: 1,
-    _id: 0,
-  });
-  
+  },
+});
+db.produtos.find({}, {
+  nome: 1,
+  ingredientes: 1,
+  _id: 0,
+});
