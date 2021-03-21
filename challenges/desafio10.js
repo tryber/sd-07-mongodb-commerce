bf20c0c0eadeff13d239935feb600c269fc8670e
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-03-21 17:22:52 -0300

    Desafios 10, 11 e 12.

---
+++ challenges/desafio10.js
@@ -0,0 +1,6 @@
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+
+db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
+
+db.produtos.find({})
+  .projection({ _id: 0, nome: 1, vendasPorDia: 1 });
