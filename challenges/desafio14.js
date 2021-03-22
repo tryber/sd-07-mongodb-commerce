fa0b4c6fee626961f22250b2abd887560a4e68b1
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-22 17:06:09 -0300

    checking challenges 10-14

---
+++ challenges/desafio14.js
@@ -0,0 +1,2 @@
+db.produtos.updateMany({ $and: [{ "valoresNutricionais.tipo": "sódio" }, { $and: [{ "valoresNutricionais.percentual": { $gt: 20, $lt: 40 } }] }] }, { $push: { tags: "contém sódio" } });
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
