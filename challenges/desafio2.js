0006fa2ffae03ef7502bc221d0fd5b0f68bb5380
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-22 16:35:14 -0300

    Exercise 2 - done-2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,3 +2,5 @@ db.produtos.updateMany(
   { valorUnitario: { $exist: false } },
   { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
+
+db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 });
