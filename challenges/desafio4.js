eb2597b4f94e263cd4a82fcfe6e54e53a7461071
Arthur de Sa <arthurdesa94@gmail.com>
2021-03-22 17:19:50 -0300

    ate o 1

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $set: {
+    ultimaModificacao: Date(),
+  } },
+);
+
+db.produtos.find(
+  { ultimaModificacao: {
+    $exists: 1,
+  } },
+  { _id: 0, nome: 1 },
+);
