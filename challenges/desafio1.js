138398d791a71fee4423c5bbbd7a162e06c0ccbf
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-22 16:09:46 -0300

    Exerxise 1 - done-6

---
+++ challenges/desafio1.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      criadoPor: "Ronald McDonald",
+    } },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    criadoPor: true,
+  },
+);
