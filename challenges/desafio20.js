df03e3826cddc558e52a8b5d222383237475838d
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-23 00:27:54 -0300

    desafio 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  {
+    $unset: {
+      curtidas: "",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: false, nome: true, curtidas: true },
+);
