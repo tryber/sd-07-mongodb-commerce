c26f5f29821018e17a58ce54bfa938ac9033cbf0
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-23 02:23:53 -0300

    req14

---
+++ challenges/desafio14.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  { "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 } },
+  {
+    $push: {
+      tags: {
+        $each: ["contém sódio"],
+        $sort: { percentual: -1 },
+      },
+    },
+  },
+);
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
