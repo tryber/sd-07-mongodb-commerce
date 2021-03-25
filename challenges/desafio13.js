fe59c140d10a55ac490a21038a003b01926ebade
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-03-25 10:55:11 -0300

    Envio dos requisitos para avaliaçao apos CodeClimate parte 3

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -4,7 +4,6 @@ db.produtos.updateMany({
       tipo: "sódio", percentual: { $gte: 40 },
     },
   },
-}, { $push: { tags: "muito sódio" } },
-);
+}, { $push: { tags: "muito sódio" } });
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
