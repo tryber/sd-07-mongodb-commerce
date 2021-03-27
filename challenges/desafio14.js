e2b5ad047b332fe5423b81417ce22c76e5c73a5a
Erling <erlingjunior@gmail.com>
2021-03-27 16:39:18 -0300

    ajustando desafio 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 } },
-  { $push: { tags: ["contém sódio"] } },
+  { $push: { tags: "contém sódio" } },
 );
 
 db.produtos.find(
