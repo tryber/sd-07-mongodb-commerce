b00db4ba453cb70d2a30e7ecee085aa7444e19a0
Felipe Farina <wfelipefarina@gmail.com>
2021-03-30 09:46:03 -0300

    Requisitos 1 ao 9, eslint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,7 @@
 db.produtos.updateMany(
-  { nome: 
-    { $in: ["Big Mac", "Quarteirão com Queijo"] }
+  { nome:
+    { $in: ["Big Mac", "Quarteirão com Queijo"] },
   },
-  { $push: { ingredientes: "bacon" } });
+  { $push: { ingredientes: "bacon" } }
+);
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,5 +1,5 @@
 db.produtos.updateOne(
   { nome: "Cheddar McMelt" },
-  { $pop: { ingredientes: 1 } }
+  { $pop: { ingredientes: 1 } },
 );
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
