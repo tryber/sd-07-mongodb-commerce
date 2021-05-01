e34b21dfae5841a466205bd8b4c7ef06606deace
André Ferreira <andreferreirap1@gmail.com>
2021-05-01 02:33:54 -0300

    Desafios 6 - 10 | Desafios 10 - pretty()

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,4 +4,4 @@ db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.4": 60 } });
 
 db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.7": 120 } });
 
-db.produtos.find({}, { _id: false, nome: true, vendasPorDia: true });
+db.produtos.find({}, { _id: false, nome: true, vendasPorDia: true }).pretty();
