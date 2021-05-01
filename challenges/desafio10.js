d5094efd1fa05925aa130b499ecff5e1751e3518
André Ferreira <andreferreirap1@gmail.com>
2021-05-01 20:18:17 -0300

    Desafio 10 | db

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
-b.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 
