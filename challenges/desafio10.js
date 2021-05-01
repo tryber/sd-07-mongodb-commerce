a30a268b4ba678a92d3cf64b03f1f15286db8619
André Ferreira <andreferreirap1@gmail.com>
2021-05-01 02:24:03 -0300

    Desafios 6 - 10 | Desafios 6 e 10 - bacon e dias da semana

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,7 +1,7 @@
 b.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
 
-db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.4": 60 } });
 
-db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.7": 120 } });
 
 db.produtos.find({}, { _id: false, nome: true, vendasPorDia: true });
